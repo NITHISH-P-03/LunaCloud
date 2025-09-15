e3ca3d5c5fb27e93af14834d5c39a110e5d8010d
Nithish <nithisp332005@gmail.com>
2025-09-15 20:54:56 +0530

    fin

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: ["http://localhost:5173", "https://lunacloudf.onrender.com"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
